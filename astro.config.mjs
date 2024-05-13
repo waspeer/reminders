7dabaed520f7a79e4aea7752f29debadda660f10
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-05-13 15:25:21 +0200

    add webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import db from '@astrojs/db';
 import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
+import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [db()],
+  integrations: [
+    db(),
+    webmanifest({
+      name: 'Niet vergeten',
+      icon: 'src/images/sun.svg',
+      short_name: 'Niet vergeten',
+      display: 'standalone',
+    }),
+  ],
   experimental: {
     actions: true,
   },
